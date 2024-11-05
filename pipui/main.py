7f75b8da98928f376b995237b6ba0167cab4d2b8
mjl <mjl@qq.com>
2024-11-05 15:50:07 +0800

    调整后端结构

--- pipui/main.py
+++ pipui/main.py
@@ -1,30 +1,25 @@
-from typing import Dict, List, Any
 from flask import Flask, request, jsonify, render_template
 import pipui
-from pipui.utils import get_installed_packages, uninstall_package, search_packages, install_package, available_packages
+from pipui.utils import PipManager, available_packages
 
 app = Flask(__name__, template_folder=pipui.__path__[0] + "/templates")
 
 
 @app.route("/interpreter-info", methods=["GET"])
 def interpreter_info():
-    import sys
-    version = sys.version_info
-    return {
-        'version': f"{version.major}.{version.minor}.{version.micro}",
-        'path': sys.executable
-    }
+    pm = PipManager()
+    return {'version': pm.version, 'path': pm.executable}
 
 
 @app.route("/", methods=["GET"])
 def read_root():
-    packages = get_installed_packages()
+    packages = PipManager().pip_list()
     return render_template("index.html", packages=packages)
 
 
 @app.route("/uninstall/<package_name>", methods=["DELETE"])
 def delete_package(package_name: str):
-    success = uninstall_package(package_name)
+    success = PipManager().pip_uninstall(package_name)
     if not success:
         return jsonify({"detail": "Package not found"}), 404
     return jsonify({"message": f"Package {package_name} uninstalled successfully"})
@@ -33,7 +28,7 @@ def delete_package(package_name: str):
 @app.route("/search/", methods=["GET"])
 def search_package():
     q = request.args.get('q')
-    # available_packages = search_packages(q)  # 替换为你的逻辑
+    available_packages = PipManager().pip_search(q)  # 替换为你的逻辑
     results = [pkg for pkg in available_packages if q.lower() in pkg["name"].lower()]
     return jsonify(results)
 
@@ -47,7 +42,7 @@ def install():
     # 在这里执行安装逻辑，比如调用系统命令安装包
     print(f"安装包：{package_details}，镜像源：{mirror_source}")
     try:
-        install_package(package_details.replace('\n', ' ').replace("\r", ' '), mirror_source)  # 假设这个函数会安装包
+        PipManager().pip_install(package_details.replace('\n', ' ').replace("\r", ' '), mirror_source)  # 假设这个函数会安装包
     except Exception as e:
         return {"msg": str(e)}
 
--- pipui/utils.py
+++ pipui/utils.py
@@ -4,79 +4,83 @@ import subprocess
 import requests
 
 
-def get_installed_packages():
-    try:
-        packages = subprocess.check_output([sys.executable, "-m", 'pip', 'list', '--format=freeze'],
-                                           stderr=subprocess.STDOUT, ).decode().replace('\r\n', '\n').split('\n')
-        return [{'name': p.split('==')[0], 'version': p.split('==')[1]} for p in packages if '==' in p]
-    except subprocess.CalledProcessError:
-        return []
+class PipManager:
+    def __init__(self):
+        self.executable = sys.executable  # 解释器路径
+        version = sys.version_info
+        self.version = f"{version.major}.{version.minor}.{version.micro}"
 
 
-def uninstall_package(package_name):
-    try:
-        subprocess.check_call([sys.executable, "-m", "pip", "uninstall", "-y", package_name])
-        return True
-    except subprocess.CalledProcessError:
-        return False
+    def pip_list(self):
+        try:
+            packages = subprocess.check_output([self.executable, "-m", 'pip', 'list', '--format=freeze'],
+                                               stderr=subprocess.STDOUT, ).decode().replace('\r\n', '\n').split('\n')
+            return [{'name': p.split('==')[0], 'version': p.split('==')[1]} for p in packages if '==' in p]
+        except subprocess.CalledProcessError:
+            return []
 
+    def pip_uninstall(self, package_name):
+        try:
+            subprocess.check_call([self.executable, "-m", "pip", "uninstall", "-y", package_name])
+            return True
+        except subprocess.CalledProcessError:
+            return False
 
-def install_package(package_name, index_url=None):
-    for i in package_name.split(' '):
-        if not i:
-            continue
-        if index_url:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", i, "--index-url", index_url])
+    def pip_install(self, package_name, index_url=None):
+        for i in package_name.split(' '):
+            if not i:
+                continue
+            i = i.split("#")[0]
+            if index_url:
+                subprocess.check_call([self.executable, "-m", "pip", "install", i, "--index-url", index_url])
+            else:
+                subprocess.check_call([self.executable, "-m", "pip", "install", i])
+
+    def pip_search_versions(self, package_name):
+        try:
+
+            # 调用 pip index versions 命令并捕获输出
+            result = subprocess.check_output(
+                [self.executable, "-m", 'pip', 'index', 'versions', package_name],
+                stderr=subprocess.STDOUT,  # 将标准错误合并到标准输出
+                # text=True  # 以文本模式返回输出
+            )
+        except subprocess.CalledProcessError as e:
+            print(f"Error: {e.output.strip()}")
+            return []
+
+        # 解析输出
+        versions = []
+        for line in result.splitlines():
+            line = line.decode()
+            if "Available versions:" in line:
+                versions = line.replace("Available versions:", "").replace(' ', '').split(',')
+                break  # 找到后就退出循环
+
+        return versions
+
+    def pip_search(self, query="a"):
+        if query:
+            response = requests.get(f'https://pypi.org/search/?q={query}')
         else:
-            subprocess.check_call([sys.executable, "-m", "pip", "install", i])
+            response = requests.get(f'https://pypi.org/search')
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, 'html.parser')
+            results = []
+            for pkg in soup.find_all('a', class_='package-snippet'):
+                name = pkg.find('span', class_='package-snippet__name').text
+                version_curr = pkg.find('span', class_='package-snippet__version').text
+                description = pkg.find('p', class_='package-snippet__description').text.strip()
+                pkg_info = {
+                    'name': name,
+                    'version': version_curr,
+                    'versions': self.pip_search_versions(name),
+                    'description': description
+                }
+                results.append(pkg_info)
+            return results
 
-
-def get_available_versions(package_name):
-    try:
-
-        # 调用 pip index versions 命令并捕获输出
-        result = subprocess.check_output(
-            [sys.executable, "-m", 'pip', 'index', 'versions', package_name],
-            stderr=subprocess.STDOUT,  # 将标准错误合并到标准输出
-            # text=True  # 以文本模式返回输出
-        )
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.output.strip()}")
         return []
-
-    # 解析输出
-    versions = []
-    for line in result.splitlines():
-        line = line.decode()
-        if "Available versions:" in line:
-            versions = line.replace("Available versions:", "").replace(' ', '').split(',')
-            break  # 找到后就退出循环
-
-    return versions
-
-
-def search_packages(query="a"):
-    if query:
-        response = requests.get(f'https://pypi.org/search/?q={query}')
-    else:
-        response = requests.get(f'https://pypi.org/search')
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        results = []
-        for pkg in soup.find_all('a', class_='package-snippet'):
-            name = pkg.find('span', class_='package-snippet__name').text
-            version_curr = pkg.find('span', class_='package-snippet__version').text
-            description = pkg.find('p', class_='package-snippet__description').text.strip()
-            pkg_info = {
-                'name': name,
-                'version': version_curr,
-                'versions': get_available_versions(name),
-                'description': description
-            }
-            results.append(pkg_info)
-        return results
-
-    return []
 
 
 available_packages = [
